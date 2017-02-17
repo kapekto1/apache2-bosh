5d4b435bf7c6fe0aa4ec521065a5868b9a5ccd46
Tomasz Kapek <tomasz.kapek@sap.com>
2017-02-17 10:48:08 +0100

    files changed

--- create-manifest.sh
+++ create-manifest.sh
@@ -4,4 +4,4 @@ export VAULT_ADDR=$VAULT_ADDR
 export VAULT_TOKEN=$VAULT_TOKEN
 export VAULT_SSL_VERIFY=$VAULT_SSL_VERIFY
 
-spruce merge apache2-bosh/apache2-deployment-stub.yml > apache2-bosh/apache2-bosh-manifest.yml
+spruce merge apache2-bosh-release/apache2-deployment-stub.yml > apache2-bosh-release/apache2-bosh-manifest.yml
